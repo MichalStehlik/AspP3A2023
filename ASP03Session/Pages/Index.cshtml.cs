457d9f61c6ad58745a7bd7bfd34881ae61a3ed3f
Michal Stehlík <michal.stehlik@pslib.cz>
2023-11-28 09:57:45 +0100

    session start

---
+++ ASP03Session/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP03Session.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        //private readonly ISession _session;
+
+        public IndexModel(ILogger<IndexModel> logger/*, ISession session*/)
+        {
+            _logger = logger;
+            //_session = session;
+        }
+
+        public void OnGet()
+        {
+            //_session.SetString("dato1", "ahoj");
+            HttpContext.Session.SetString("dato2","nazdar");
+        }
+    }
+}
---
+++ ASP03Session/Pages/Privacy.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP03Session.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        //private readonly ISession _session;
+
+        public string Data1 { get; set; }
+        public string Data2 { get; set; }
+
+        public PrivacyModel(ILogger<PrivacyModel> logger/*, ISession session */)
+        {
+            _logger = logger;
+            //_session = session;
+        }
+
+        public void OnGet()
+        {
+            Data1 = HttpContext.Session.GetString("dato2") ?? "none";
+            //Data2 = _session.GetString("dato2") ?? "nothing";
+        }
+    }
+}
